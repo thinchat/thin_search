7aba52bda7fc1b55e8b624d936d9fa59df8ba172
Edward Weng <edweng@gmail.com>
2012-06-06 21:19:51 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinSearch::Application.config.secret_token = 'c6e567e22dee3eb6475bc2ff09998dbc9155c69d729271ed3ab481a5ae963341066dc4b5ffa87002fd018af54481356fbd0b5cc95a2431dc20618883d2ec1406'
