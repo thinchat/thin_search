a0499bfbfcf83c793cc1b62cb52322560222431f
Edward Weng <edweng@gmail.com>
2012-06-12 16:27:01 -0400

    Updating redis urls

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,14 @@
-REDIS = Redis.new(:host => 'localhost', :port => 6379)
-require "#{Rails.root}/config/initializers/secret.rb"
-REDIS.auth(REDIS_PASSWORD) if Rails.env == 'production'
+if Rails.env == 'production'
+  REDIS_URL = 'http://thinchat.com'
+elsif Rails.env == 'staging'
+  REDIS_URL = 'http://50.116.40.131'
+else
+  REDIS_URL = 'localhost'
+end
+
+REDIS = Redis.new(:host => REDIS_URL, :port => 6379)
+
+if Rails.env == 'production'
+  require "#{Rails.root}/config/secret/redis_password.rb"
+  REDIS.auth(REDIS_PASSWORD)
+end
