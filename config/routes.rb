70a00c223e5dd2a3170af4751f3f728d9952f50d
Chris Anderson <christopher.anderson@gmail.com>
2012-06-13 13:39:42 -0400

    Changed routes for nginx conf

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 
-
 ThinSearch::Application.routes.draw do
   mount Resque::Server.new, :at => "/resque"
-  
-  namespace :api do
-    namespace :v1 do
-      resource :search
+  scope '/search' do
+    namespace :api do
+      namespace :v1 do
+        resource :search
+      end
     end
   end
 end
